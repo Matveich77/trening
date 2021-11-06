1e86cb7a540c055b6c85e21f765de64246659ba1
Мария <matveich77@mail.ru>
2021-11-06 12:03:45 +0300

    get second program

--- src/main/java/LessonTwo/SecondProgram.java
+++ src/main/java/LessonTwo/SecondProgram.java
@@ -1,16 +1,28 @@
 package LessonTwo;
 
 public class SecondProgram {
-long r;
-long arg2; long arg1;
+long r; long arg2; long arg1;
 public static void main (String[] args){
     System.out.println("Привет, мир!");
-    double result1=result(4);
-    System.out.println(result1);
 
-    long summa1=summa(2, 10);
+    double result1=result(8);
+    System.out.println(result1);
+    /**Метод будет вычислять площадь круга из заданного значения радиуса r
+     *
+     * @param r радиус окружности
+     * @return вычисление площади
+     */
+
+    long summa1=summa(28, 62);
     System.out.println(summa1);
 }
+
+    /** Метод будет вычислять сумму двух чисел
+     *
+     * @return результат сложения
+     */
+
+
     public static double result (long r){
         double result=r*r*3.14;
         return result;
